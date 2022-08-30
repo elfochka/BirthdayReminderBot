e0e227d7e5ce37f2312609455a06b9cd12a88d61
elfochka <elfochka@bk.ru>
2022-08-31 00:13:48 +0300

    v 2.0 от 30.08.2022

--- SuperBot.py
+++ SuperBot.py
@@ -1,88 +1,31 @@
-# -*- coding: utf-8 -*
+from subprocess import check_output
+from subprocess import check_call
 
-import telebot  # Импорт талебота
-from telebot import types
+import telebot
+import time
 
-print('Hello, Elfochka')
+print('SupeBot запущен')
 
-bot = telebot.TeleBot('5407469548:AAH66oGKqUE5PWm-dOGCixXk0sZWRMlyglE')  # Подключение моего бота по токену
-
-# Дальше мой код:
-
-
+bot = telebot.TeleBot("5407469548:AAH66oGKqUE5PWm-dOGCixXk0sZWRMlyglE")  # токен бота
+user_id = 142351451  # id вашего аккаунта
 
 
-name = ''
-surname = ''
-age = 0
+@bot.message_handler(content_types=["text"])
+def main(message):
+    if (user_id == message.chat.id):  # проверяем, что пишет именно владелец
+        comand = message.text[1:]  # текст сообщения
+
+        # bot.send_message(message.chat.id, comand)
+
+        try:  # если команда невыполняемая - check_output выдаст exception
+            bot.send_message(message.chat.id, check_output(comand, shell=True))
+        except:
+            bot.send_message(message.chat.id, "Invalid input")  # если команда некорректна
 
 
-@bot.message_handler(content_types=['text'])
-def start(message):
-    if message.text == '/reg':
-        bot.send_message(message.from_user.id, "Как тебя зовут?")
-        bot.register_next_step_handler(message, get_name)  # следующий шаг – функция get_name
-    else:
-        bot.send_message(message.from_user.id, 'Напиши /reg')
-
-
-def get_name(message):  # получаем фамилию
-    global name
-    name = message.text
-    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
-    bot.register_next_step_handler(message, get_surname)
-
-
-def get_surname(message):
-    global surname
-    surname = message.text
-    bot.send_message(message.from_user.id, 'Сколько тебе лет?')
-    bot.register_next_step_handler(message, get_age)
-
-
-def get_age(message):
-    global age
-    age = message.text  # проверяем, что возраст введен корректно
-
-    # try:
-    #     age = int(message.text) #проверяем, что возраст введен корректно
-    # except Exception:
-    #     bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
-    #     get_surname(message)
-
-    keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
-    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes') #кнопка «Да»
-    keyboard.add(key_yes); #добавляем кнопку в клавиатуру
-    key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
-    keyboard.add(key_no);
-    question = 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?';
-    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
-        # .... #код сохранения данных, или их обработки
-        bot.send_message(call.message.chat.id, 'Запомню : )');
-    elif call.data == "no":
-         # ... #переспрашиваем
-        bot.send_message(call.message.chat.id, 'Ну тогда я пошел : )');
-
-
-bot.polling(none_stop=True, interval=0)
-
-
-
-
-#
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-#
-#
+if __name__ == '__main__':
+    while True:
+        try:  # добавляем try для бесперебойной работы
+            bot.polling(none_stop=True)  # запуск бота
+        except:
+            time.sleep(10)  # в случае падения
